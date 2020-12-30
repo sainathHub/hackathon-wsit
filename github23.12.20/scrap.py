57c781592c0b357f741ef3cf00e353380306e35b
sainathHub <33556716+sainathHub@users.noreply.github.com>
2020-12-30 09:56:56 +0530

    Add files via upload

--- github23.12.20/scrap.py
+++ github23.12.20/scrap.py
@@ -3,6 +3,8 @@ from urllib.request import urlopen
 from urllib.request import urlopen
 import requests
 import pandas as pd
+from pandas import ExcelWriter
+
 import math
 import csv
 
@@ -20,79 +22,120 @@ def clean(data):
 
     return new_data
 
+def gh(row):
+    ret = []
+    if 'plus' in row[0][0]:
+         ret.append(row[0][1])
+         for i in range(1,len(row)):
+            ret.append(row[i][0]+row[i][1])
+    else:
+        ret.append(row[0][0])
+        for i  in range(1,len(row)):
+            ret.append(row[i][0]+row[i][1])
+        
+    return tuple(ret)
+
+
 def compare_report(oldfdata, exceldata):
-    oldfdata.sort()
-    print(oldfdata)
     hash_map = {}
-    hash_map1 = {}
-    missing = []
+
+
+
     header = ['Type',	'ReasonForGAndE',	'name'	,'Organization'	,'Role','location in web sheet']
-
+    
     for i,row in enumerate(oldfdata):
-        
-        hash_map1[tuple(row)] = (True, i) 
-    
-    for i,row in enumerate(exceldata):
-        hash_map[tuple(row)] = (True, i)
+        hash_map[gh(row)] = (True, i)
 
-    keys = hash_map1.keys()
+    keys = hash_map.keys()
     for i, row in enumerate(exceldata):
-        if  tuple(row)  in keys:
-            row.append(hash_map1[tuple(row)][1]+2)
-            if len(row) < 5:
-                ind = row[-1]
-                row[-1] = ""
-                row.extend(["" for _ in range(len(header) - len(row))])
-                row[-1] = ind
-        else :
-            row.append("missing")
-
-
-    exceldata.insert(0, header)
+        if  gh(row)  in keys:
+            row[0].append(hash_map[gh(row)][1]+1)
     
-        
-    with open('reports/report.csv','w',newline="") as File:
-        writer = csv.writer(File)
-        writer.writerows(exceldata)
+        else :
+            row[o].append('Missing')
+
+   # exceldata.insert(0, header)
+    
+
+    expanded = []
+    for row in exceldata:
+        expanded.extend(row)
+
+    df1 = pd.DataFrame(expanded,
+                   columns=header)
+
+    with ExcelWriter(r'C:\Users\siddh\Documents\github23.12.20\out.xlsx') as writer:
+        df1.to_excel(writer)
 
 
+def get_pluscount(rows, i):
+    pluscount = 0
+    for row in rows[i+1:]:
+        if len(row) < 4:
+            pluscount = pluscount + 1
+        else :
+            break
+    return pluscount
 
-
-URL = 'C:/Users/siddh/Desktop/1.html'
+URL = r'C:\Users\siddh\Documents\github23.12.20\static.html'
 soup = BeautifulSoup(open(URL), "html.parser")
 data = []
 table = soup.find('table')
 table_body = table.find('tbody')
 
 rows = table_body.find_all('tr')
-for row in rows:
+for i, row in enumerate(rows):
     cols = row.find_all('td')
     cols = [ele.text.strip() for ele in cols]
-    
     data.append([ele for ele in cols if ele]) # Get rid of empty values
-   
 
+
+data.remove(data[0])
+for i,row in enumerate(data):
+    if row[0] == "Forename":
+        data.remove(row)
 
 oldf_data = []
-i = 1
-while(i < len(data)):
-    row = data[i]
-    newrow = [row[1], row[2],row[3],row[4],row[5]]
-    oldf_data.append(newrow)
+
+i=0
+while i < len(data):
+    entry = []
+    if "plus" in data[i][0] :
+        entry.append(data[i][1:-3])
+    pluscount = get_pluscount(data, i)
+  
     i = i+1
-
-    if  "Plus 1(1)" == row[0] :
-        i= i+1 #skipping the hreader and current
-        row = data[i]
-        oldf_data.append(["Plus 1",row[0],row[1]])
-        i=i+2 #skipping the space and current
-
+    if pluscount > 0:
+        for j in range(pluscount):
+            entry.append(["Plus 1", data[i][0], data[i][1]])
+            i=i+1
+    oldf_data.append(entry)
 
 print(oldf_data)
-print()
-df = pd.read_excel(r'Book1.xlsx',sheet_name="Sheet1")
+
+df = pd.read_excel(r'C:\Users\siddh\Documents\github23.12.20\Book1.xlsx',sheet_name="Sheet1")
 excel_data = df.values.tolist()
 excel_data = clean(excel_data)
-print(excel_data)
 
-compare_report(oldf_data, excel_data)
+new_excel = []
+
+data = excel_data
+i=0
+while i < len(data):
+    entry = []
+    if "Plus" not in data[i][0] :
+        entry.append(data[i] )
+    pluscount = get_pluscount(data, i)
+  
+    i = i+1
+    if pluscount > 0:
+        for j in range(pluscount):
+            entry.append(data[i])
+            i=i+1
+    new_excel.append(entry)
+
+
+
+print(new_excel)
+
+compare_report(oldf_data, new_excel)
